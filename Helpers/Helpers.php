3478e5a7865bda212866929123d36d42f6c7e6a9
esthefania <estefa.naravez.26@gmail.com>
2024-04-13 22:01:58 -0500

    Diseño del Templete

--- Helpers/Helpers.php
+++ Helpers/Helpers.php
@@ -10,6 +10,18 @@
 
             return BASE_URL."/Assets";
     }
+function headerAdmin($data="")
+{
+    $view_header = "Views/Template/header_admin.php";
+    require_once ($view_header);
+}
+
+function footerAdmin($data="")
+{
+    $view_footer = "Views/Template/footer_admin.php";
+    require_once ($view_footer);
+}
+
 //Muestra la informacion formateada o mas lejible
     function dep($data)
     {
---
+++ Views/Template/header_admin.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <!-- Descripcion de la pagina al buscar -->
+    <meta name="description" content="Vali is a responsive and free admin theme built with Bootstrap 5, SASS and PUG.js. It's fully customizable and modular.">
+  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Juan Sebastian Madrid, Esthefania Narvaez">
+    <meta name="theme-color" content="#009688">
+    <link rel="shortcut icon" href="<?=media();?>/images/favicon.ico">
+    <title><?= $data['page_tag']?></title>
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="<?=media(); ?>/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?=media(); ?>/css/style.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  </head>
+  <body class="app sidebar-mini">
+    <!-- Navbar-->
+    <header class="app-header"><a class="app-header__logo" href="<?= base_url();?>/dashboard">BoGallery</a>
+      <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+      <!-- Navbar Right Menu-->
+      <ul class="app-nav">
+        
+        <!-- Notification Menu
+        <li class="dropdown"><a class="app-nav__item" href="#" data-bs-toggle="dropdown" aria-label="Show notifications"><i class="bi bi-bell fs-5"></i></a>
+          <ul class="app-notification dropdown-menu dropdown-menu-right">
+            <li class="app-notification__title">You have 4 new notifications.</li>
+            <div class="app-notification__content">
+              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><i class="bi bi-envelope fs-4 text-primary"></i></span>
+                  <div>
+                    <p class="app-notification__message">Lisa sent you a mail</p>
+                    <p class="app-notification__meta">2 min ago</p>
+                  </div></a></li>
+              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><i class="bi bi-exclamation-triangle fs-4 text-warning"></i></span>
+                  <div>
+                    <p class="app-notification__message">Mail server not working</p>
+                    <p class="app-notification__meta">5 min ago</p>
+                  </div></a></li>
+              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><i class="bi bi-cash fs-4 text-success"></i></span>
+                  <div>
+                    <p class="app-notification__message">Transaction complete</p>
+                    <p class="app-notification__meta">2 days ago</p>
+                  </div></a></li>
+              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><i class="bi bi-envelope fs-4 text-primary"></i></span>
+                  <div>
+                    <p class="app-notification__message">Lisa sent you a mail</p>
+                    <p class="app-notification__meta">2 min ago</p>
+                  </div></a></li>
+              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><i class="bi bi-exclamation-triangle fs-4 text-warning"></i></span>
+                  <div>
+                    <p class="app-notification__message">Mail server not working</p>
+                    <p class="app-notification__meta">5 min ago</p>
+                  </div></a></li>
+              <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><i class="bi bi-cash fs-4 text-success"></i></span>
+                  <div>
+                    <p class="app-notification__message">Transaction complete</p>
+                    <p class="app-notification__meta">2 days ago</p>
+                  </div></a></li>
+            </div>
+            <li class="app-notification__footer"><a href="#">See all notifications.</a></li>
+          </ul>
+        </li> -->
+        <!-- User Menu-->
+        <li class="dropdown"><a class="app-nav__item" href="#" data-bs-toggle="dropdown" aria-label="Open Profile Menu"><i class="bi bi-person fs-4"></i></a>
+          <ul class="dropdown-menu settings-menu dropdown-menu-right">
+            <li><a class="dropdown-item" href="<?= base_url();?>/opciones"><i class="bi bi-gear me-2 fs-5"></i> Configuraciones</a></li>
+            <li><a class="dropdown-item" href="<?= base_url();?>/perfil"><i class="bi bi-person me-2 fs-5"></i> Perfil</a></li>
+            <li><a class="dropdown-item" href="<?= base_url();?>/logout"><i class="bi bi-box-arrow-right me-2 fs-5"></i> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </header>
+    <?php require_once("nav_admin.php");?>
