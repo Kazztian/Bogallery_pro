db8dcc920680895590dd19d72e2d70984bf3d5c5
esthefania <estefa.naravez.26@gmail.com>
2024-04-26 19:32:15 -0500

    meloo

--- Assets/js/functions_roles.js
+++ Assets/js/functions_roles.js
@@ -1,48 +1,46 @@
-
- var tableRoles;
+var tableRoles;
 
 document.addEventListener('DOMContentLoaded', function(){
-   
-    
-    script.onload = function() {
-        tableRoles = $('#tableRoles').dataTable({
-            "aProcessing": true,
-            "aServerSide": true,
-            "language": {
-                "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
-            },
-            "ajax": {
-                "url":""+base_url+"/roles/getRoles",
-                "dataSrc": ""
-            },
-            "columns": [
-                {"data": "id_rol"},
-                {"data": "nombrerol"},
-                {"data": "descripcion"},
-                {"data": "status"},
-                {"data": "options"}
-            ],
-            "responsive": true,
-            "bDestroy": true,  
-            "iDisplayLength": 10,   
-            "order": [[0, "desc"]]
-        });
+    tableRoles = $('#tableRoles').dataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        "ajax": {
+            "url":""+base_url+"/Roles/getRoles",
+            "dataSrc": ""
+        },
+        "columns": [
+            {"data": "id_rol"},
+            {"data": "nombrerol"},
+            {"data": "descripcion"},
+            {"data": "status"},
+            {"data": "options"}
+        ],
+        "resonsieve": true,
+        "bDestroy": true,  
+        "iDisplayLength": 10,   
+        "order": [[0, "desc"]]
+    });
 
-        // Creacion de un nuevo rol
-        var formRol = document.querySelector("#formRol");
-        formRol.onsubmit = function(e){
-            e.preventDefault(); //prevenir que se recargue la pagina
+    // Creacion de un nuevo rol
 
-            var strNombre = document.querySelector('#txtNombre').value;
-            var strDesc = document.querySelector('#txtDesc').value;
-            var strStatus = document.querySelector('#listStatus').value;
-            if(strNombre =='' || strDesc =='' || strStatus ==''){
-                // Utilizamos SweetAlert en lugar de swal()
-                Swal.fire("Atención", "Todos los campos son obligatorios.", "error");
-                return false;
-            }
+    var formRol = document.querySelector("#formRol");
+    formRol.onsubmit = function(e){
+        e.preventDefault(); //prevenir que se recargue la pagina
+
+        var strNombre = document.querySelector('#txtNombre').value;
+        var strDesc = document.querySelector('#txtDesc').value;
+        var strStatus = document.querySelector('#listStatus').value;
+        if(strNombre =='' || strDesc =='' || strStatus ==''){
+        swal("Atencion","Todos los campos son obligatorios.","Erro");
+        return false;
         }
-    };
+
+
+
+    }
 });
 
 $('#tableRoles').DataTable();
