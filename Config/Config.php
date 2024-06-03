14027f120660366ab3cd29db6172fbadb9f807d9
esthefania <estefa.naravez.26@gmail.com>
2024-06-03 11:11:25 -0500

    Se actualizo todo hasta usurios

--- Config/Config.php
+++ Config/Config.php
@@ -4,7 +4,7 @@
   //define("BASE_URL", "http://localhost/bogallery_pro");
 
 
-  const BASE_URL = "http://localhost/bogallery_pro";
+  const BASE_URL = "http://localhost:8080/Bogallery_pro";
   //Zona horaria
   date_default_timezone_set('America/Bogota');
 
