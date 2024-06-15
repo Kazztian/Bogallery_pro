1312708cdbfe2cc85d93cb0efd4893eca665287b
esthefania <estefa.naravez.26@gmail.com>
2024-06-15 17:03:51 -0500

    video 82 miro lo del login

--- Assets/js/functions_login.js
+++ Assets/js/functions_login.js
@@ -4,50 +4,68 @@ script.src = "https://cdn.jsdelivr.net/npm/sweetalert2@9";
 document.head.appendChild(script);
 
 document.addEventListener('DOMContentLoaded', function() {
-    if (document.querySelector("#formCambiarPass")) {
-        let formCambiarPass = document.querySelector("#formCambiarPass");
-        formCambiarPass.onsubmit = function(e) {
+    if (document.querySelector("#formLogin")) {
+        let formLogin = document.querySelector("#formLogin");
+        formLogin.onsubmit = function(e) {
             e.preventDefault();
 
-            let strPassword = document.querySelector('#txtPassword').value;
-            let strPasswordConfirm = document.querySelector('#txtPasswordConfirm').value;
-            let id_usuario = document.querySelector('#id_usuario').value;
+            let email = document.querySelector('#txtEmail').value;
+            let password = document.querySelector('#txtPassword').value;
 
-            if (strPassword == "" || strPasswordConfirm == "") {
-                Swal.fire("Por favor", "Ingrese la nueva contraseña.", "error");
+            if (email == "" || password == "") {
+                Swal.fire("Por favor", "Ingrese su usuario y contraseña.", "error");
                 return false;
-            } else {
-                if (strPassword.length < 5) {
-                    Swal.fire("Atención", "La contraseña debe tener al menos 5 caracteres.", "info");
-                    return false;
-                }
+            }
 
-                if (strPassword != strPasswordConfirm) {
-                    Swal.fire("Atencion", "Las contraseñas no son iguales.", "info");
-                    return false;
-                }
-
-                var request = (window.XMLHttpRequest) ?
-                    new XMLHttpRequest() :
-                    new ActiveXObject('Microsoft.XMLHTTP');
-                var ajaxUrl = base_url + '/Login/setPassword';
-                var formData = new FormData(formCambiarPass);
-                request.open("POST", ajaxUrl, true);
-                request.send(formData);
-                request.onreadystatechange = function() {
-                    if (request.readyState != 4) return;
-                    if (request.status == 200) {
-                        var objData = JSON.parse(request.responseText);
-                        if (objData.status) {
-                            Swal.fire("Éxito", objData.msg, "success").then(function() {
-                                window.location = base_url + '/login';
-                            });
-                        } else {
-                            Swal.fire("Atención", objData.msg, "error");
-                        }
+            var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+            var ajaxUrl = base_url + '/Login/loginUser';
+            var formData = new FormData(formLogin);
+            request.open("POST", ajaxUrl, true);
+            request.send(formData);
+            request.onreadystatechange = function() {
+                if (request.readyState != 4) return;
+                if (request.status == 200) {
+                    var objData = JSON.parse(request.responseText);
+                    if (objData.status) {
+                        window.location = base_url + '/dashboard';
                     } else {
-                        Swal.fire("Atención", "Error en el proceso.", "error");
+                        Swal.fire("Atención", objData.msg, "error");
                     }
+                } else {
+                    Swal.fire("Atención", "Error en el proceso.", "error");
+                }
+            }
+        }
+    }
+    
+    if (document.querySelector("#formRecetPass")) {
+        let formRecetPass = document.querySelector("#formRecetPass");
+        formRecetPass.onsubmit = function(e) {
+            e.preventDefault();
+
+            let emailReset = document.querySelector('#txtEmailReset').value;
+
+            if (emailReset == "") {
+                Swal.fire("Por favor", "Ingrese su email.", "error");
+                return false;
+            }
+
+            var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+            var ajaxUrl = base_url + '/Login/resetPass';
+            var formData = new FormData(formRecetPass);
+            request.open("POST", ajaxUrl, true);
+            request.send(formData);
+            request.onreadystatechange = function() {
+                if (request.readyState != 4) return;
+                if (request.status == 200) {
+                    var objData = JSON.parse(request.responseText);
+                    if (objData.status) {
+                        Swal.fire("Éxito", objData.msg, "success");
+                    } else {
+                        Swal.fire("Atención", objData.msg, "error");
+                    }
+                } else {
+                    Swal.fire("Atención", "Error en el proceso.", "error");
                 }
             }
         }
