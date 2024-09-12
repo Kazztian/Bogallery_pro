76e340ebc8e080ba66446eb9c5e666c80e31d744
esthefania <estefa.naravez.26@gmail.com>
2024-09-11 20:18:01 -0500

    pagina nosotros y contactoo seguir

--- Config/Config.php
+++ Config/Config.php
@@ -3,12 +3,12 @@
   //define("BASE_URL", "http://localhost:8080/Bogallery_pro");
   //define("BASE_URL", "http://localhost/bogallery_pro");
 
-  const BASE_URL = "http://localhost/bogallery_pro";
+  const BASE_URL = "http://localhost:8080/Bogallery_pro";
   //Zona horaria
   date_default_timezone_set('America/Bogota');
 
   const DB_HOST = "localhost";
-  const DB_NAME = "pro_bogallery";
+  const DB_NAME = "bogallery_pro";
   const DB_USER = "root";
   const DB_PASSWORD = "";
   const DB_CHARSET = "charset=utf8";
